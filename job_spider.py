e9c83f709b58d0d12fb4e9a44d455130a380961a
rubinliudongpo <rubinliudongpo@users.noreply.github.com>
2017-09-18 08:31:24 +0800

    fixed bugs with routines post_require and post_desc_counter

--- job_spider.py
+++ job_spider.py
@@ -1,26 +1,32 @@
 from pprint import pprint
 import csv
 from collections import Counter
-import requests
+
 from bs4 import BeautifulSoup
-import jieba
+import requests
 import matplotlib.pyplot as plt
+import jieba
 from wordcloud import WordCloud
+import os
+import re
 
 class JobSpider():
 
     def __init__(self):
         self.company = []
         self.text = ""
-        self.headers = {'X-Requested-With': 'XMLHttpRequest',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                      'Chrome/56.0.2924.87 Safari/537.36'}
+        self.headers = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/56.0.2924.87 Safari/537.36'
+        }
 
     def job_spider(self):
         """ 爬虫入口 """
-        url = "http://search.51job.com/list/010000%252C020000%252C030200%252C040000,000000,0000,00,9,99,Python,2,{}.html?" \
-              "lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0" \
-              "&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
+        url = "http://search.51job.com/list/010000%252C020000%252C030200%252C040000,000000" \
+              ",0000,00,9,99,Python,2,{}.html? lang=c&stype=1&postchannel=0000&workyear=99&" \
+              "cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0" \
+              "&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
         urls = [url.format(p) for p in range(1, 14)]
         for url in urls:
             r = requests.get(url, headers=self.headers).content.decode('gbk')
@@ -30,7 +36,7 @@ class JobSpider():
                     href, post = b.find('a')['href'], b.find('a')['title']
                     locate = b.find('span', class_='t3').text
                     salary = b.find('span', class_='t4').text
-                    d = {'href':href, 'post':post, 'locate':locate, 'salary':salary}
+                    d = {'href': href, 'post': post, 'locate': locate, 'salary': salary}
                     self.company.append(d)
                 except Exception:
                     pass
@@ -39,30 +45,39 @@ class JobSpider():
         """ 爬取职位描述 """
         for c in self.company:
             r = requests.get(c.get('href'), headers=self.headers).content.decode('gbk')
-            bs = BeautifulSoup(r, 'lxml').find('div', class_="bmsg job_msg inbox").text
-            s = bs.replace("举报", "").replace("分享", "").replace("\t", "").strip()
-            self.text += s
+            try:
+                bs = BeautifulSoup(r, 'lxml').find('div', class_="bmsg job_msg inbox").text
+                s = bs.replace("举报", "").replace("分享", "").replace("\t", "").strip()
+                self.text += s
+            except AttributeError:
+                break
         # print(self.text)
-        with open(r".\data\post_require.txt", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_require.txt")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f.write(self.text)
 
     def post_desc_counter(self):
         """ 职位描述统计 """
         # import thulac
-        post = open(r".\data\post_require.txt", "r", encoding="utf-8").read()
+        file_path = os.path.join("data", "post_require.txt")
+        post = open(file_path, "r", encoding="utf-8").read()
         # 使用 thulac 分词
         # thu = thulac.thulac(seg_only=True)
         # thu.cut(post, text=True)
 
         # 使用 jieba 分词
-        jieba.load_userdict(r".\data\user_dict.txt")
+        file_path = os.path.join("data", "user_dict.txt")
+        jieba.load_userdict(file_path)
         seg_list = jieba.cut(post, cut_all=False)
         counter = dict()
+        pattern = r"[`~!@#$%^&*()_\-+=<>?:\"{}|,\.\/;'\\[\]·~！@#￥%……&*（）——\-+={}|《》？：“”【】、；‘’，。、\r\n\s]"
         for seg in seg_list:
-            counter[seg] = counter.get(seg, 1) + 1
+            if not re.match(pattern, seg, re.VERBOSE):
+                counter[seg] = counter.get(seg, 1) + 1
         counter_sort = sorted(counter.items(), key=lambda value: value[1], reverse=True)
         pprint(counter_sort)
-        with open(r".\data\post_pre_desc_counter.csv", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_pre_desc_counter.csv")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f_csv = csv.writer(f)
             f_csv.writerows(counter_sort)
 
@@ -72,7 +87,8 @@ class JobSpider():
         counter = Counter(lst)
         counter_most = counter.most_common()
         pprint(counter_most)
-        with open(r".\data\post_pre_counter.csv", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_pre_counter.csv")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f_csv = csv.writer(f)
             f_csv.writerows(counter_most)
 
@@ -82,7 +98,8 @@ class JobSpider():
         for c in self.company:
             lst.append((c.get('salary'), c.get('post'), c.get('locate')))
         pprint(lst)
-        with open(r".\data\post_salary_locate.csv", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary_locate.csv")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f_csv = csv.writer(f)
             f_csv.writerows(lst)
 
@@ -91,7 +108,8 @@ class JobSpider():
         mouth = []
         year = []
         thouand = []
-        with open(r".\data\post_salary_locate.csv", "r", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary_locate.csv")
+        with open(file_path, "r", encoding="utf-8") as f:
             f_csv = csv.reader(f)
             for row in f_csv:
                 if "万/月" in row[0]:
@@ -104,43 +122,52 @@ class JobSpider():
         calc = []
         for m in mouth:
             s = m[0].split("-")
-            calc.append((round((float(s[1]) - float(s[0])) * 0.4 + float(s[0]), 1), m[1], m[2]))
+            calc.append(
+                (round((float(s[1]) - float(s[0])) * 0.4 + float(s[0]), 1), m[1], m[2]))
         for y in year:
             s = y[0].split("-")
-            calc.append((round(((float(s[1]) - float(s[0])) * 0.4 + float(s[0])) / 12, 1), y[1], y[2]))
+            calc.append(
+                (round(((float(s[1]) - float(s[0])) * 0.4 + float(s[0])) / 12, 1), y[1], y[2]))
         for t in thouand:
             s = t[0].split("-")
-            calc.append((round(((float(s[1]) - float(s[0])) * 0.4 + float(s[0])) / 10, 1), t[1], t[2]))
+            calc.append(
+                (round(((float(s[1]) - float(s[0])) * 0.4 + float(s[0])) / 10, 1), t[1], t[2]))
         pprint(calc)
-        with open(r".\data\post_salary.csv", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary.csv")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f_csv = csv.writer(f)
             f_csv.writerows(calc)
 
     def post_salary_counter(self):
         """ 薪酬统计 """
-        with open(r".\data\post_salary.csv", "r", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary.csv")
+        with open(file_path, "r", encoding="utf-8") as f:
             f_csv = csv.reader(f)
             lst = [row[0] for row in f_csv]
         counter = Counter(lst).most_common()
         pprint(counter)
-        with open(r".\data\post_salary_counter1.csv", "w+", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary_counter1.csv")
+        with open(file_path, "w+", encoding="utf-8") as f:
             f_csv = csv.writer(f)
             f_csv.writerows(counter)
 
     def world_cloud(self):
         """ 生成词云 """
         counter = {}
-        with open(r".\data\post_desc_counter.csv", "r", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_pre_desc_counter.csv")
+        with open(file_path, "r", encoding="utf-8") as f:
             f_csv = csv.reader(f)
             for row in f_csv:
                 counter[row[0]] = counter.get(row[0], int(row[1]))
             pprint(counter)
-        wordcloud = WordCloud(font_path=r".\font\msyh.ttf",
+        file_path = os.path.join("font", "msyh.ttf")
+        wordcloud = WordCloud(font_path=file_path,
                               max_words=100, height=600, width=1200).generate_from_frequencies(counter)
         plt.imshow(wordcloud)
         plt.axis('off')
         plt.show()
-        wordcloud.to_file('.\images\worldcloud.jpg')
+        file_path = os.path.join("images", "worldcloud.jpg")
+        wordcloud.to_file(file_path)
 
     def insert_into_db(self):
         """ 插入数据到数据库 
@@ -151,9 +178,15 @@ class JobSpider():
             );
         """
         import pymysql
-        conn = pymysql.connect(host="localhost", port=3306, user="root", passwd="0303", db="chenx", charset="utf8")
+        conn = pymysql.connect(host="localhost",
+                               port=3306,
+                               user="root",
+                               passwd="0303",
+                               db="chenx",
+                               charset="utf8")
         cur = conn.cursor()
-        with open(r".\data\post_salary.csv", "r", encoding="utf-8") as f:
+        file_path = os.path.join("data", "post_salary.csv")
+        with open(file_path, "r", encoding="utf-8") as f:
             f_csv = csv.reader(f)
             sql = "insert into jobpost(j_salary, j_locate, j_post) values(%s, %s, %s)"
             for row in f_csv:
@@ -165,11 +198,16 @@ class JobSpider():
                     print(e)
         cur.close()
 
+
 if __name__ == "__main__":
     spider = JobSpider()
-    # spider.job_spider()
-    # spider.post_salary()
-    # spider.insert_into_db()
-    # spider.post_salary_counter()
-    # spider.post_counter()
-    # spider.world_cloud()
+    spider.job_spider()
+    # 按需启动
+    spider.post_require()
+    spider.post_desc_counter()
+    spider.post_salary_locate()
+    spider.post_salary()
+    spider.insert_into_db()
+    spider.post_salary_counter()
+    spider.post_counter()
+    spider.world_cloud()
